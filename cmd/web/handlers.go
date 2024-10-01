d40791ce5d3226c0038bc209b11fe1e60edfbea1
Vladimir Solovyov <leilyngale@icloud.com>
2024-10-01 13:41:32 +0800

    Scaffold the app

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,55 +3,51 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		app.notFound(w)
+		http.NotFound(w, r)
 		return
 	}
+
 	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/partials/nav.html",
-		"./ui/html/pages/home.html",
+		"./ui/html/base.htm",
+		"./ui/html/partials/nav.htm",
+		"./ui/html/partials/footer.htm",
+		"./ui/html/pages/home.htm",
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.serverError(w, err)
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.serverError(w, err)
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 0 {
-		app.notFound(w)
+		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	fmt.Fprintf(w, "Displaying a specific snippet with ID: %d...", id)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
-	title := "0 snail"
-	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-	expires := 7
-
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	w.Write([]byte("Creating a new snippet...)"))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,61 +1,22 @@
 package main
 
 import (
-	"database/sql"
-	"flag"
-	"github.com/Dimisz/snippetbox/internal/models"
-	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
-	"os"
 )
 
-type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *models.SnippetModel
-}
+const PORT = ":4000"
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", "springstudent:springstudent@/snippetbox?parseTime=true", "MySQL data source name")
+	mux := http.NewServeMux()
 
-	flag.Parse()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-
-	db, err := openDB(*dsn)
-	if err != nil {
-		errorLog.Fatal(err)
-	}
-
-	defer db.Close()
-
-	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippets: &models.SnippetModel{DB: db},
-	}
-
-	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
-	}
-
-	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
-	errorLog.Fatal(err)
-}
-
-func openDB(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
-	return db, nil
+	log.Printf("Starting server on port%s...", PORT)
+	err := http.ListenAndServe(PORT, mux)
+	log.Fatal(err)
 }
