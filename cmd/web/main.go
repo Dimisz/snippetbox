4cd41556701cb760b30dd81f43102c1f39ac0ce0
Vladimir Solovyov <leilyngale@icloud.com>
2025-04-28 10:19:12 +0800

    Set up MySQL

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,11 +18,20 @@ type application struct {
 func main() {
 	// command-line flag to customize port
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	// command-line flag for MySQL DSN str
+	dsn := flag.String("dsn", "username:password@/snippetbox?parseTime=true", "MySQL datasource name")
 	flag.Parse()
 
 	// custom logger
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// initialize our application
 	app := &application{
@@ -35,6 +47,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	// pool initialized but no connections yet
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	// that's why need to ping to verify
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
