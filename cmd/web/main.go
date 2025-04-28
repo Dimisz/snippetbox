e94bdfe796924f34b2033873d9bf57ff6b3444aa
Vladimir Solovyov <leilyngale@icloud.com>
2025-04-28 10:47:38 +0800

    Scaffold DBC handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,15 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/dimisz/snippetbox/internal/models"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -37,6 +40,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	// custom server struct to channel all error logs thru custom logger
