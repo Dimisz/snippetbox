1440ddcd343c1256e33b4525e1684a36b50828e6
Vladimir Solovyov <leilyngale@icloud.com>
2025-05-17 08:08:20 +0800

    Add logging middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -16,3 +16,14 @@ func secureHeaders(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// logRequest uses our custom infoLogger
+// logs the following details of HTTP requests:
+// - IP address of the user
+// - which URL and method are being requested
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
