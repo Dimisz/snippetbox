cd084e1eb7178decfa92f80b2e9631dcb2ad45dc
Vladimir Solovyov <leilyngale@icloud.com>
2025-09-17 14:56:01 +0800

    Set up Signup page and scaffold basic validators

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,12 +11,45 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type userSignupForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Display a HTML form for signing up a new user...")
+	data := app.newTemplateData(r)
+	data.Form = userSignupForm{}
+	app.render(w, http.StatusOK, "signup.html", data)
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Create a new user...")
+	var form userSignupForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+
+	// if any errors
+	// redisplay the signup form along with 422
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+		return
+	}
+
+	// otherwise send a placeholder
+	fmt.Fprintln(w, "Create a new user...")
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,9 +1,12 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9])(?:[a-zA-Z0-9-]){")
 
 type Validator struct {
 	FieldErrors map[string]string
@@ -33,6 +36,11 @@ func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
+// returns true if a value contains at least n characters
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
 // returns true if value contains no more than
 func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
@@ -45,4 +53,9 @@ func PermittedInt(value int, permittedValues ...int) bool {
 		}
 	}
 	return false
+}
+
+// returns true if a value matches a provided regexp
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
