932db27b9c8f2d259e0a8ce962fa69ed1443a0b6
Vladimir Solovyov <leilyngale@icloud.com>
2026-02-03 22:07:29 +0800

    Extract routes into a separate file

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -8,7 +8,9 @@ import (
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Print(trace)
+	// set depth to 2 to see the original line
+	// that caused the error
+	app.errorLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
